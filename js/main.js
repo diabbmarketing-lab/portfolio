b9383f0acb866c11e6dc09fcd4301a6eca6e37bc
Ayman-monn <ayman10112013@gmail.com>
2025-09-20 23:07:39 +0300

    Pic View

--- js/main.js
+++ js/main.js
@@ -1,6 +1,41 @@
 const contentBox = document.getElementById("project-card");
 const toggleBtn = document.getElementById("toggleBtn");
 
+
+// Start overlay
+    const pics = document.querySelectorAll(".pic");
+    const overlay = document.getElementById("overlay");
+    const overlayImg = document.getElementById("overlay-img");
+
+    // Open overlay when clicking any image
+    pics.forEach(pic => {
+      pic.addEventListener("click", () => {
+        overlay.style.display = "flex";
+        overlayImg.src = pic.src;
+      });
+    });
+
+    // Close overlay when clicking anywhere
+    overlay.addEventListener("click", () => {
+      overlay.style.display = "none";
+    });
+
+// End OverLay
+// Read More Button 
+const readMoreBtn = document.querySelector(".read-more-btn");
+const projectMeta = document.querySelector(".project-meta");
+
+readMoreBtn.addEventListener("click", () => {
+  projectMeta.classList.toggle("expanded");
+
+  if (projectMeta.classList.contains("expanded")) {
+    readMoreBtn.textContent = "Read Less";
+  } else {
+    readMoreBtn.textContent = "Read More";
+  }
+});
+
+// Read More and Less Button
 toggleBtn.addEventListener("click", () => {
   contentBox.classList.toggle("project-card-extend");
   toggleBtn.textContent = contentBox.classList.contains("project-card-extend")
